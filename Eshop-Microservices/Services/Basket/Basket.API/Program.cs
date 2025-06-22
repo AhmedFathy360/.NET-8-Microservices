22a3735a705bb72880010ea24dc333b6979057e3
Ahmed Fathy <ahmedfathey555@gmail.com>
2025-06-22 16:05:35 +0300

    Consuming Discount Grpc Service From Basket Microservice When Adding Cart Item

--- Eshop-Microservices/Services/Basket/Basket.API/Program.cs
+++ Eshop-Microservices/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,10 @@
+using Discount.Grpc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Application Services
 var assembly = typeof(Program).Assembly;    
 builder.Services.AddCarter();
 
@@ -28,6 +32,23 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
     //options.InstanceName = "Basket";
 });
+
+// gRPC Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+}).ConfigurePrimaryHttpMessageHandler(() =>
+    {
+        var handler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback =
+            HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+        // This is not recommended for production use, as it bypasses SSL certificate validation.
+        return handler;
+    });
+
+// Crross-Cutting Services
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 builder.Services.AddHealthChecks()
